ec0c1e1d7e13ead9b4558aabbf552f877594125c
Jon <jon@jgubman.com>
2015-12-15 17:30:32 -0800

    first pass

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # videojs-airplay
-Videojs Plugin that adds an airplay button to the control bar
+Videojs Plugin that adds an airplay button to the control bar if the browser supports the WebKitPlaybackTargetAvailabilityEvent (currently Safari 9.0)
+
+## NOTES
+Built for video.js 5.0 and above.
+
+## Usage
+
+Include:
+* `videojs-airplayButton.css`
+* `videojs-airplayButton.js`
+
+```js
+  videojs('example-video', plugins: { airplayButton: {} });
+```
+
+## License
+
+Licesned Apache 2. See LICENSE file.
---
+++ src/videojs-airplay.js
@@ -0,0 +1,52 @@
+import videojs from 'video.js';
+const Component = videojs.getComponent('Component');
+const Button = videojs.getComponent('Button');
+
+class AirplayToggle extends Button {
+
+  constructor(player, options) {
+    super(player, options);
+  }
+
+  buildCSSClass() {
+    return `vjs-airplay-control ${super.buildCSSClass()}`;
+  }
+
+  handleClick() {
+    this.player().el().getElementsByTagName('video')[0].webkitShowPlaybackTargetPicker()
+  }
+
+}
+
+AirplayToggle.prototype.controlText_ = 'Airplay';
+videojs.registerComponent('AirplayToggle', AirplayToggle);
+
+const AirplayButton = function(options) {
+  let player = this;
+
+  if (window.WebKitPlaybackTargetAvailabilityEvent) {
+    let videoEl = player.el().getElementsByTagName('video')[0];
+
+    videoEl.addEventListener('webkitplaybacktargetavailabilitychanged', function(event) {
+      switch (event.availability) {
+        case "available":
+        console.log("airplay available");
+
+        player.on('loadeddata', function() {
+          if (!player.controlBar.childNameIndex_.hasOwnProperty('AirplayToggle')) {
+            var AirplayToggle = this.controlBar.addChild('AirplayToggle', options);
+            player.controlBar.el().insertBefore(AirplayToggle.el(), player.controlBar.fullscreenToggle.el());
+          }
+        });
+        break;
+
+        case "not-available":
+        console.log("airplay not available");
+        break;
+      }
+    });
+
+  };
+};
+
+videojs.plugin('airplayButton', AirplayButton);
